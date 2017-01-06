2f4c7533259abc97d02bfe4117c96e2f2f9278ef
Josephus Yang <Josephus@Josephuss-MBP.local>
2017-01-06 11:18:06 +0800

    add upload params for upload

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ var AWS = require('aws-sdk');
 var request = require('request');
 const stream = require('stream');
 
-var uploadToS3 = function uploadFromStream(s3, cb) {
+
+var uploadToS3 = function uploadFromStream(s3, uploadParams, cb) {
   var pass = new stream.PassThrough();
-  var params = { Body: pass};
+
+  var params = upuploadParams || {};
+  params.Body = pass;
   s3.upload(params).
-  on('httpUploadProgress', function(evt) { console.log(evt); }).
+  // on('httpUploadProgress', function(evt) { console.log(evt); }).
   send(function(err, data) { 
     console.log(err, data); 
     if (err) cb(err, data);
-    cb(null, data.Location);// data.Location is the uploaded location
+    if (data && data.Location)
+      cb(null, data.Location);// data.Location is the uploaded location
+    else 
+      cb (new Error("data.Location not found!"), data);
   });
   return pass;
 };
-var uploadToS3RawResponse = function uploadFromStream(s3, cb) {
+var uploadToS3RawResponse = function uploadFromStream(s3, uploadParams, cb) {
   var pass = new stream.PassThrough();
-  var params = { Body: pass};
+  var params = upuploadParams || {};
+  params.Body = pass;
   s3.upload(params).
-  on('httpUploadProgress', function(evt) { console.log(evt); }).
+  // on('httpUploadProgress', function(evt) { console.log(evt); }).
   send(function(err, data) { 
     console.log(err, data); 
 	cb(err, data);
@@ -28,8 +35,8 @@ var uploadToS3RawResponse = function uploadFromStream(s3, cb) {
 /* initiate a get request to the url and pipe the data to S3 bucket
  * will terminate when statusCode != 200
  * params: 
- *	url : the url of the target resource
- *	bucketName: the bucketName in S3
+ *  url : the url of the target resource
+ *  bucketName: the bucketName in S3
  *  itemKey: the fileName on S3
  *  callback: function(err, data), where data will Location in upload response
  */
@@ -46,17 +53,38 @@ exports.urlToS3 = function(url, bucketName, itemKey, callback) {
     }
   });
 }
-
-/* Same as urlToS3, but callback will return s3 upload raw response
- *  callback: function(err, data), where data is s3 upload response
+/* initiate a get request to the url and pipe the data to S3 bucket
+ * will terminate when statusCode != 200
+ * params: 
+ *  url : the url of the target resource
+ *  bucketName: the bucketName in S3
+ *  itemKey: the fileName on S3
+ *  callback: function(err, data), where data will Location in upload response
  */
-exports.urlToS3WithRawResponse = function(url, bucketName, itemKey, callback) {
-  var s3 = new AWS.S3({params : {Bucket: bucketName, Key: itemKey}});
+exports.urlToS3WithS3Obj = function(url, s3Obj, uploadParams, callback) {
+  var s3 = s3Obj; 
   var req = request.get(url);
   req.pause();
   req.on('response', function(resp) {
     if (resp.statusCode == 200) {
-      req.pipe(uploadToS3RawResponse(s3, callback));
+      req.pipe(uploadToS3(s3, uploadParams, callback));
+      req.resume();
+    } else {
+      callback(new Error('request item did not respond with HTTP 200'));
+    }
+  });
+}
+
+/* Same as urlToS3, but callback will return s3 upload raw response
+ *  callback: function(err, data), where data is s3 upload response
+ */
+exports.urlToS3RawWithS3Obj = function(url, s3Obj, uploadParams, callback) {
+  var s3 = s3Obj;
+  var req = request.get(url);
+  req.pause();
+  req.on('response', function(resp) {
+    if (resp.statusCode == 200) {
+      req.pipe(uploadToS3RawResponse(s3, uploadParams, callback));
       req.resume();
     } else {
       callback(new Error('request item did not respond with HTTP 200'));
